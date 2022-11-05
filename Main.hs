eff8ed017e96d5ed6b2b77b8f0e9a4a72e06db29
Kamil Muzyka <62958895+kamilmuzyka91@users.noreply.github.com>
2022-11-05 12:56:18 +0000

    Operatory

--- Main.hs
+++ Main.hs
@@ -2,6 +2,13 @@ imie = "Kamil"
 x = 5
 
 jestDUZEB y = y == 'B' -- funkcja
+jestDODATNIA a = a > 0 -- czy liczba jest dodatnia
+jestUJEMNA c = c < 0
+jestZEREM d = d == 0
+
+zwiekszOjeden d = d + 1
+zSUMUJ a b = a + b
+
 
 main :: IO ()
 main = do
@@ -11,5 +18,10 @@ main = do
   print $ x
   print $ jestDUZEB 'B' -- wywołanie funkcji zwróci TRUE
   print $ jestDUZEB 'b' -- zwróci false
+  print $ jestDODATNIA 5 
 {- komentarz wieloliniowy -}
-
+  -- ŹLE print $ jestUJEMNA -10
+  print $ jestUJEMNA (-1)
+  print $ jestZEREM 0
+  print $ zwiekszOjeden 2
+  print $ zSUMUJ 2 3
