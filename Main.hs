e256fab81a5293d87ee96896fa74d272332e7906
Kamil Muzyka <62958895+kamilmuzyka91@users.noreply.github.com>
2022-11-05 12:43:19 +0000

    deklaracja funkcji

--- Main.hs
+++ Main.hs
@@ -1,6 +1,15 @@
+imie = "Kamil"
+x = 5
+
+jestDUZEB y = y == 'B' -- funkcja
+
 main :: IO ()
 main = do
   print $ "tom"
   print $ "Dom" -- komentarz jednoliniowy
-
+  print $ "Mam na imie " ++ imie
+  print $ x
+  print $ jestDUZEB 'B' -- wywołanie funkcji zwróci TRUE
+  print $ jestDUZEB 'b' -- zwróci false
 {- komentarz wieloliniowy -}
+
